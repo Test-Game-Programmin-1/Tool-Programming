1483ff7a15f5db12fbb2d88abb1934c64e8e6009
Deva <manuelbocelli1@gmail.com>
2026-07-31 16:51:17 +0200

    fifth push

--- Assets/Editor/ToolBuilderW_.cs
+++ Assets/Editor/ToolBuilderW_.cs
@@ -1,23 +1,70 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
 
 public class ToolBuilderW_ : EditorWindow
 {
+    #region tool builder window
+
     [MenuItem("Tools/House Builder")]
     public static void ShowWindow()
     {
         GetWindow<ToolBuilderW_>("House Builder Tool");
     }
+    #endregion
+
     private int DiscSize;
     private bool visibleArea;
     Vector3 prevPos;
+    class CategoryData
+    {
+        public string Name;
+        public string[] RoomPath;
+        public string[] RoomName;
+        public GameObject[] RoomAssets;
+    }
+    const string path = "Assets/Prefab";
+    readonly List<CategoryData> categoryList = new();
+    CategoryData selectedCategory;
     private void OnEnable()
     {
+        FoldScanner();
         SceneView.duringSceneGui += OnSceneGUI;
     }
     private void OnDisable()
     {
         SceneView.duringSceneGui -= OnSceneGUI;
+    }
+    private void FoldScanner()
+    {
+        categoryList.Clear();
+        selectedCategory = null;
+        if(!AssetDatabase.IsValidFolder(path)) return;
+        string RoomFolderPath = Path.GetFullPath(path);
+        string[] Dirs = Directory.GetDirectories(RoomFolderPath);
+        foreach (string dir in Dirs)
+        {
+            string categoryName = Path.GetFileName(dir);
+            string assetFolderPath = path + "/" + categoryName;
+            string[] guids = AssetDatabase.FindAssets("t:Prefab", new[] { assetFolderPath });
+            if (guids.Length ==0) continue;
+
+            var categoryDatas = new CategoryData
+            {
+                Name = categoryName,
+                RoomPath = new string[guids.Length],
+                RoomName = new string[guids.Length],
+                RoomAssets = new GameObject[guids.Length]
+            };
+            for (int i = 0; i < guids.Length; i++)
+            {
+                categoryDatas.RoomPath[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+                categoryDatas.RoomName[i] = Path.GetFileNameWithoutExtension(categoryDatas.RoomPath[i]);
+                categoryDatas.RoomAssets[i] = AssetDatabase.LoadAssetAtPath<GameObject>(categoryDatas.RoomPath[i]);
+            }
+            categoryList.Add(categoryDatas);
+        }
     }
     private void OnGUI()
     {
@@ -69,31 +116,17 @@ public class ToolBuilderW_ : EditorWindow
 
         GUILayout.Space(10);
 
-        if(GUILayout.Button("1 Door"))
+        foreach (CategoryData category in categoryList)
         {
-            // Handle 1 Door button click
+            if(GUILayout.Button(category.Name))
+            {
+                selectedCategory = category;
+            }
         }
-
-        GUILayout.Space(10);
-
-        if(GUILayout.Button("2 Doors"))
-        {
-            // Handle 2 Doors button click
-        }
-
-        GUILayout.Space(10);
-
-        if(GUILayout.Button("3 Doors"))
-        {
-            // Handle 3 Doors button click
-        }
-
-        GUILayout.Space(10);
-
-
     }
     void OnSceneGUI(SceneView sceneView)
     {
+        ButtonOver();
         if(!visibleArea)return;
         Event e = Event.current;
         Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
@@ -108,5 +141,29 @@ public class ToolBuilderW_ : EditorWindow
             }
         }
         sceneView.Repaint();
+    }
+    void ButtonOver()
+    {
+        float buttonSize = 100f;
+        Handles.BeginGUI();
+        GUILayout.BeginArea(new Rect(10, 10 ,100, Screen.height - 20)); 
+        if(selectedCategory != null)
+        {
+            for (int i = 0; i < selectedCategory.RoomAssets.Length; i++)
+            {
+                GameObject prefab = selectedCategory.RoomAssets[i];
+                Texture2D preview = AssetPreview.GetAssetPreview(prefab);
+                GUIContent content;
+                if(preview != null) content = new GUIContent(preview, selectedCategory.RoomName[i]);
+                else content = new GUIContent(selectedCategory.RoomName[i]);
+                if(GUILayout.Button(content, GUILayout.Width(buttonSize), GUILayout.Height(buttonSize)))
+                {
+                    
+                }
+            }
+        }
+        GUILayout.EndArea();
+        Handles.EndGUI();
+        SceneView.RepaintAll();
     }
 }
